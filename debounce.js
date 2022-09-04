c49aa5628c6836c05236856d8f679ca1f960cb19
FrankQiu <iam.frankqiu@gmail.com>
2022-09-04 23:22:34 +0800

    feat: submit some code

---
+++ debounce.js
@@ -0,0 +1,27 @@
+reference: https://github.com/sisterAn/JavaScript-Algorithms/issues/95
+
+// fn 是需要防抖处理的函数
+// wait 是时间间隔
+function debounce(fn, wait = 50) {
+    // 通过闭包缓存一个定时器 id
+    let timer = null
+    // 将 debounce 处理结果当作函数返回
+    // 触发事件回调时执行这个返回函数
+    return function(...args) {
+        // this保存给context
+        const context = this
+      	// 如果已经设定过定时器就清空上一次的定时器
+        if (timer) clearTimeout(timer)
+      
+      	// 开始设定一个新的定时器，定时器结束后执行传入的函数 fn
+        timer = setTimeout(() => {
+            fn.apply(context, args)
+        }, wait)
+    }
+}
+
+// DEMO
+// 执行 debounce 函数返回新函数
+const betterFn = debounce(() => console.log('fn 防抖执行了'), 1000)
+// 停止滑动 1 秒后执行函数 () => console.log('fn 防抖执行了')
+document.addEventListener('scroll', betterFn)
